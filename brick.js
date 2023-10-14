96b7d5b9a947ca4a4f993c90d12781828af67a63
Hardik <hardik2213105@akgec.ac.in>
2023-10-14 20:07:56 +0530

    ball(breaker) bounces on touching sidewalls

--- brick.js
+++ brick.js
@@ -8,6 +8,20 @@ let playerwidth = 80;
 let playerheight = 10;
 let playervelocityX = 10;
 
+//Breaker
+let ballwidth = 10;
+let ballheight = 10;
+let ballvelocityX = 3;
+let ballvelocityY = 2;
+
+let ball = {
+    x:boardwidth/2,
+    y:boardheight/2,
+    height : ballheight,
+    width : ballwidth,
+    velocityX : ballvelocityX,
+    velocityY : ballvelocityY
+}
 
 let player = {
     x:boardwidth/2 -playerwidth/2,
@@ -32,15 +46,63 @@ window.onload = function(){
 function update(){
     requestAnimationFrame(update);
     context.clearRect(0,0,board.width,board.height);
-
+    //player
     context.fillStyle = "lightgreen";
     context.fillRect(player.x, player.y, player.width, player.height);
+
+    //Breaker
+    context.fillStyle = "white";
+    ball.x += ball.velocityX;
+    ball.y += ballvelocityY;
+    context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+    //bounce
+    if(ball.y <= 0){
+        // if ball touches top
+        ball.velocityY *= -1;
+    }
+    else if(ball.x <= 0 || (ball.x + ball.width) >= boardwidth){
+        // if ball touches of left or right 
+        ball.velocityX *= -1;
+    }
+    else if((ball.y + ball.height) >= boardheight){
+        
+    }
+}
+function outofbound(xPosition){
+    return(xPosition <0 || xPosition + playerwidth > boardwidth);
 }
 function movePlayer(e){
     if(e.code == "ArrowLeft"){
-        player.x -= player.velocityX;
+        //player.x -= player.velocityX;
+        let nextplayerX = player.x - player.velocityX;
+        if(!outofbound(nextplayerX)){
+            player.x = nextplayerX;
+        }
     }
     else if (e.code == "ArrowRight"){
-        player.x += player.velocityX;
+       // player.x += player.velocityX;
+       let nextplayerX = player.x + player.velocityX;
+        if(!outofbound(nextplayerX)){
+            player.x = nextplayerX;
+        }
     }
+}
+function detectcollision(a,b){
+    return a.x < b.x +b.width && 
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+}
+function topCollision(ball,block){ //a is ball
+    return detectcollision(ball,block) && (ball.y + ball.height) >= block.y;
+}
+function bottemCollision(ball,block){
+    return detectcollision(ball,block) && (block.y + block.height) >= ball.y;
+}
+function leftCollision(ball,block){
+    return detectcollision(ball,block) && (ball.x + ball.width) >= block.x;
+}
+function righttCollision(ball,block){
+    return detectcollision(ball,block) && (block.x + block.width) >= ball.x;
 }
